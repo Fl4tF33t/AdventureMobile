f4f1399ac41d6f157609b78b2a852ace1aad8892
Fl4tF33t <89871056+Fl4tF33t@users.noreply.github.com>
2024-09-25 17:04:05 +0300

    testing the movement

--- godotmobilegame/Scene/Player/player.gd
+++ godotmobilegame/Scene/Player/player.gd
@@ -1,21 +1,26 @@
 extends CharacterBody2D
 
+const SPEED:= 100
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	handle_inputs(delta)
+	handle_animation()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func handle_inputs(delta):
+	var direction_x
+	direction_x = Input.get_axis("left", "right")
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+
+func handle_animation():
+	if velocity.x != 0:
+		animated_sprite.flip_h = true if velocity.x < 0 else false
+		animated_sprite.play("side_walk")
+	if velocity.x == 0:
+		animated_sprite.play("side_idle")
